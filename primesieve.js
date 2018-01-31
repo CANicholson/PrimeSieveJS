7f1e638cf4b5742e327d8b15ad0eb145bdcc50ca
Unknown <canicholson96@gmail.com>
2018-01-31 17:25:35 +0000

    For loops added
    Added For loops and array to verify if a number is a multipe. Made the local true/false array to state if an index(which is the prime number in this case) should be added to the prime array. Current issue is that array stops at [2,3]

--- primesieve.js
+++ primesieve.js
@@ -1,13 +1,24 @@
-function primegen(expectedprimes, primes){
-    var upperlimit = 100;
-    var primes = [2];
-    while (primes.length != expectedprimes){
-
-    }
+function primegen(expectedprimes){
+    var upperlimit = 100; //This will be incremented until we find our expected number of primes
+    var primes = []; //Initially empty so we don't add 2 twice
+    //while (primes.length != expectedprimes){
+        var tf = new Array();
+            for(i = 2; i<=upperlimit; i++){
+            tf.push(true);
+            }
+            for (i = 2; i<=Math.sqrt(upperlimit); i++){
+                if (tf[i]){
+                    for (j = i*i; j <= upperlimit; j++){
+                        tf[j] = false;
+                    }         
+                primes.push(i);
+                }
+            }
+    //}
     return primes;
 }
 
-var expectedprimes = 25;
+var expectedprimes = 5;
 
 var prime = primegen(expectedprimes);
-print (primes);
+console.log(prime);

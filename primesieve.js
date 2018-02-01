3394c8c41d53279703eeef694a08dc13059d9898
Unknown <canicholson96@gmail.com>
2018-02-01 15:57:30 +0000

    All primes past initial segment size calculated. Algorithm complete
    Eliminate all numbers that are multiples of our previous primes. Prime algorithm complete

--- primesieve.js
+++ primesieve.js
@@ -1,29 +1,38 @@
 function primegen(expectedprimes){
     var upperlimit = 100; //This will be incremented until we find our expected number of primes
     var primes = []; //Initially empty so we don't add 2 twice
-    var lastnumber = 2
-    var tf = new Array();
+    var lastnumber = 2;
+    var past1stsegment = false;
     while (primes.length != expectedprimes){
+            var tf = new Array();
+
             for(i = lastnumber; i<=upperlimit; i++){
             if (i == 2 || i%2 != 0) //Optimise by only adding odds
                 tf[i] = true;
             else
                 tf[i] = false;
             }
+            if(past1stsegment){
+                for (i = 0; i<=primes.length; i++){
+                    for (j = primes[i]*primes[i]; j <= upperlimit; j += primes[i]){
+                        tf[j] = false;
+                    }     
+                }
+            }
             for (i = lastnumber; i<=upperlimit; i++){
                 if (tf[i]){
                     for (j = i*i; j <= upperlimit; j += i){
                         tf[j] = false;
-                    }         
+                    }     
                 primes.push(i);
                 }
             if (primes.length == expectedprimes) //program breaks when the expected # of primes is found
-                break;
-            }
-        lastnumber = tf.length - 1;
+                return primes;
+        }
+        past1stsegment = true;
+        lastnumber = tf.length -1;
         upperlimit += 100;
     }
-    return primes;
 }
 
 var expectedprimes = 50;

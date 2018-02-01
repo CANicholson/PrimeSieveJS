dddff7195c7ef3f66f612ad26ce91a6abeb14fb8
Unknown <canicholson96@gmail.com>
2018-02-01 13:21:18 +0000

    Fixed adding only odd number
    The previous version of the if statement added true even if the number was even as I forgot to make the else add a false

--- primesieve.js
+++ primesieve.js
@@ -1,14 +1,16 @@
 function primegen(expectedprimes){
-    var upperlimit = 200; //This will be incremented until we find our expected number of primes
+    var upperlimit = 10000; //This will be incremented until we find our expected number of primes
     var primes = []; //Initially empty so we don't add 2 twice
+    var lastnumber = 2
+    var tf = new Array();
     while (primes.length != expectedprimes){
-        var tf = new Array();
-            for(i = 2; i<=upperlimit; i++)
-            if (i==2 || i%2 != 0) //Optimise by only adding odds
-                tf.push(true);
+            for(i = lastnumber; i<=upperlimit; i++){
+            if (i == 2 || i%2 != 0) //Optimise by only adding odds
+                tf[i] = true;
             else
-                tf.push(true);
-            for (i = 2; i<=Math.sqrt(upperlimit); i++){
+                tf[i] = false;
+            }
+            for (i = lastnumber; i<=Math.sqrt(upperlimit); i++){
                 if (tf[i]){
                     for (j = i*i; j <= upperlimit; j += i){
                         tf[j] = false;
@@ -18,11 +20,13 @@ function primegen(expectedprimes){
             if (primes.length == expectedprimes) //program breaks when the expected # of primes is found
                 break;
             }
+        lastnumber = Math.sqrt(tf.length-1);
+        upperlimit += 100 
     }
     return primes;
 }
 
-var expectedprimes = 5;
+var expectedprimes = 26;
 
 var prime = primegen(expectedprimes);
 console.log(prime);

0af8879a60ba95321d4d8118f05ff931f55773ca
Unknown <canicholson96@gmail.com>
2018-02-02 20:58:09 +0000

    Formatted table

--- primesieve.js
+++ primesieve.js
@@ -60,8 +60,8 @@ outside = function(){
     var multiplication = new Table({
         chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
         , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-        , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-        , 'right': '' , 'right-mid': '' , 'middle': ' ' },
+        , 'left': '' , 'left-mid': '' , 'mid': '-' , 'mid-mid': '-'
+        , 'right': '' , 'right-mid': '' , 'middle': '|' },
         style: { 'padding-left': 0, 'padding-right': 0 }
     })
     var fs = require('fs');
